52c0bafe6eacf3c49d8560dad39c03c622d61416
khushalkanoje547-design <khushalkanoje547@gmail.com>
2026-08-09 22:52:40 +0530

    Add JS code and Updated HTML and CSS file

---
+++ srcipt.js
@@ -0,0 +1,37 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turnO = true;
+const winPatterns = [
+    [0, 1, 2] ,
+    [0, 3, 6] ,
+    [0, 4, 8] ,
+    [1, 4, 7] ,
+    [2, 4, 6] ,
+    [2, 5, 8] ,
+    [3, 4, 5] ,
+    [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click" , () => {
+        console.log("box was clicked");
+
+        if(turnO) { // PlayerO turn 
+            box.innerText = "O";
+            turnO = false;
+        }else{ //palyerX turn
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const checkWinner = () =>{
+    for(pattern of winPatterns) {
+        console.log(pattern);
+    }
+}
